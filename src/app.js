4e4febee4580ab24fdd0d6499d93b48e6219b8e5
Sophie Aubert <sophie.aubert@heig-vd.ch>
2023-02-07 15:24:55 +0100

    Affichage - Ok

---
+++ src/app.js
@@ -0,0 +1,39 @@
+"use strict"
+const placeAfficheage = document.querySelector("main");
+
+
+const recupDonnees = fetch(`https://randomuser.me/api/?results=20`)
+.then((res) => res.json()
+.then((recupDonnees) => console.log(recupDonnees)));
+
+const recupDonnee = () => {
+fetch(`https://randomuser.me/api/?results=20`)
+.then((resultat) => {
+    return resultat.json();
+})
+.then((data) => {
+    data.results.forEach((element) => affichePersonne(element));
+})
+}
+recupDonnee();
+
+const affichePersonne = (personne) => {
+// ON MODIFIE LE DOM : 
+const persoElement = document.createElement("div");
+persoElement.classList.add("user");
+const childHTML = `
+<div class="user" data-present="false">
+<img src=${personne.picture.thumbnail}>
+<div class="user--info">
+        <h1>${personne.name.title} ${personne.name.first} ${personne.name.last}</h1>
+        <p>${personne.registered.age} years old</p>
+        <p>${personne.location.city}, ${personne.location.country}</p>
+        <a href=mailto:${personne.email}>
+				<span class="mail">✉️</span>
+		</a>
+</div>`
+
+
+placeAfficheage.insertAdjacentHTML("afterbegin", childHTML);
+// return persoElement;
+}
